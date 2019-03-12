a8dd8612fbb5641a43f823179253164082d16e40
Daniel Perez <danielperezvl@gmail.com>
2019-03-12 15:25:17 +0000

    Solution with tdd small tests

--- src/roman.test.ts
+++ src/roman.test.ts
@@ -1,4 +1,32 @@
-import { romanToDecimal } from './roman';
+import { romanToDecimal, getSingleDecimalFromRoman } from './roman';
+
+it('can convert I to 1', () => {
+  expect(romanToDecimal('I')).toEqual(1);
+});
+
+it('can convert III to 3', () => {
+  expect(romanToDecimal('III')).toEqual(3);
+});
+
+it('can convert V to 5', () => {
+  expect(romanToDecimal('V')).toEqual(5);
+});
+
+it('can convert VI to 6', () => {
+  expect(romanToDecimal('VI')).toEqual(6);
+});
+
+it('can convert VIII to 8', () => {
+  expect(romanToDecimal('VIII')).toEqual(8);
+});
+
+it('can convert IV to 4', () => {
+  expect(romanToDecimal('IV')).toEqual(4);
+});
+
+it('can convert IX to 9', () => {
+  expect(romanToDecimal('IX')).toEqual(9);
+});
 
 it('converts advanced roman numbers to decimal', () => {
   expect(romanToDecimal('MMVI')).toEqual(2006);
@@ -6,4 +34,30 @@ it('converts advanced roman numbers to decimal', () => {
 
 it('converts more advanced roman numbers to decimal', () => {
   expect(romanToDecimal('MCMXLIV')).toEqual(1944);
+});
+
+it('converts more advanced roman numbers to decimal', () => {
+  expect(romanToDecimal('LXXXVIII')).toEqual(88);
+});
+
+it('converts more advanced roman numbers to decimal', () => {
+  expect(romanToDecimal('CMXCIX')).toEqual(999);
+});
+
+describe('get single roman number', () => {
+  it('gets I', () => {
+    expect(getSingleDecimalFromRoman('I')).toEqual(1)
+  });
+
+  it('gets 5', () => {
+    expect(getSingleDecimalFromRoman('V')).toEqual(5)
+  });
+
+  it('gets 10', () => {
+    expect(getSingleDecimalFromRoman('X')).toEqual(10)
+  });
+
+  it('defaults to 0', () => {
+    expect(getSingleDecimalFromRoman('sadklfjasdklfjsadklf')).toEqual(0);
+  })
 });
--- src/roman.ts
+++ src/roman.ts
@@ -1,3 +1,40 @@
-export function romanToDecimal () {
+export function getSingleDecimalFromRoman (char: string) {
+  switch (char) {
+    case 'I':
+      return 1;
+    case 'V':
+      return 5;
+    case 'X':
+      return 10;
+    case 'L':
+      return 50;
+    case 'C':
+      return 100;
+    case 'D':
+      return 500;
+    case 'M':
+      return 1000;
+    default:
+      return 0;
+  }
+}
 
+export function romanToDecimal (rom: string) {
+  const romList = rom.split('');
+  const decList = romList.map(letter => {
+    return getSingleDecimalFromRoman(letter);
+  });
+
+  let acum = 0;
+
+  decList.forEach((num, index, arr) => {
+    if (num !== 0 && num < arr[index + 1])  {
+      acum += arr[index + 1] - num;
+      arr[index + 1] = 0;
+    } else {
+      acum += num;
+    }
+  });
+
+  return acum;
 }
